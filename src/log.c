82605a950a13cd83413f6faaa8345de8de657649
fard-draf <fard@ik.me>
2026-06-10 16:26:59 +0200

    log: setlogmask to ignore LOG_DEBUG

--- src/log.c
+++ src/log.c
@@ -4,5 +4,6 @@ const char *SYSLOG_NAME = "canreader";
 
 void syslog_init(void) {
     openlog(SYSLOG_NAME, LOG_CONS | LOG_NDELAY, LOG_DAEMON);
+    setlogmask(LOG_UPTO(LOG_INFO));
     syslog(LOG_INFO, "[SYSTEM] canreader's log enabled");
 }
