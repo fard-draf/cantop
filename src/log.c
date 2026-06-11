f05f61febb5670344ae6122b1e0b96604f9795dc
fard-draf <fard@ik.me>
2026-06-11 09:30:33 +0200

    log: fix app name

--- src/log.c
+++ src/log.c
@@ -1,9 +1,9 @@
 #include "../include/log.h"
 
-const char *SYSLOG_NAME = "canreader";
+const char *SYSLOG_NAME = "cantop";
 
 void syslog_init(void) {
     openlog(SYSLOG_NAME, LOG_CONS | LOG_NDELAY, LOG_DAEMON);
     setlogmask(LOG_UPTO(LOG_INFO));
-    syslog(LOG_INFO, "[SYSTEM] canreader's log enabled");
+    syslog(LOG_INFO, "[SYSTEM] %s's log enabled", SYSLOG_NAME);
 }
