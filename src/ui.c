321e8e69c8b08286972233e2ec52e8bdaa3686f9
fard-draf <fard@ik.me>
2026-06-12 14:40:51 +0200

    ui: add pgns
    still messy, work in progress

--- src/ui.c
+++ src/ui.c
@@ -2,7 +2,7 @@
 
 #define WIN_TITLE_H 3
 #define WIN_GLOBAL_H 5
-#define WIN_PROVIDERS_H 8
+#define WIN_PROV_H 8
 #define WIN_TRAMES_H 10
 #define WIN_WIDTH 60
 #define WIN_START_X 2
@@ -13,6 +13,9 @@ static WINDOW *win_providers = NULL;
 static WINDOW *win_trames = NULL;
 
 void ui_draw(Analyzer *an);
+int compute_y(int slot);
+void ui_provider_add(int provider_id);
+void ui_provider_remove(int provider_id);
 
 int ui_init(AppContext *ac) {
     int tfd = timerfd_create(CLOCK_MONOTONIC, TFD_CLOEXEC);
@@ -47,11 +50,11 @@ int ui_init(AppContext *ac) {
 
     win_title = newwin(WIN_TITLE_H, WIN_WIDTH, 1, WIN_START_X);
     win_global = newwin(WIN_GLOBAL_H, WIN_WIDTH, WIN_TITLE_H + 1, WIN_START_X);
-    win_providers = newwin(WIN_PROVIDERS_H, WIN_WIDTH,
+    win_providers = newwin(WIN_PROV_H, WIN_WIDTH,
                            WIN_TITLE_H + WIN_GLOBAL_H + 1, WIN_START_X);
-    win_trames = newwin(WIN_TRAMES_H, WIN_WIDTH,
-                        1 + WIN_TITLE_H + WIN_GLOBAL_H + WIN_PROVIDERS_H + 1,
-                        WIN_START_X);
+    win_trames =
+        newwin(WIN_TRAMES_H, WIN_WIDTH,
+               1 + WIN_TITLE_H + WIN_GLOBAL_H + WIN_PROV_H + 1, WIN_START_X);
 
     return 0;
 }
@@ -97,19 +100,18 @@ void ui_draw(Analyzer *an) {
     box(win_global, 0, 0);
     mvwprintw(win_global, 0, 2, " Global ");
     mvwprintw(win_global, 1, 2, "tram_count : %d",
-              an->metr.glob_data_rate.tram_count_total);
+              an->metr.datas.tram_count_total);
     mvwprintw(win_global, 2, 2, "since      : %.1fs",
               elapsed_ms(&an->metr.start) / 1000.0);
-    mvwprintw(win_global, 3, 2, "tram/sec   : %d",
-              an->metr.glob_data_rate.tram_rate);
+    mvwprintw(win_global, 3, 2, "tram/sec   : %d", an->metr.datas.tram_rate);
 
     // --- fenêtre trames ---
     werase(win_providers);
     box(win_providers, 0, 0);
     mvwprintw(win_providers, 0, 2, " Providers ");
     int j = 1;
-    for (uint8_t i = an->prov_mgmt.instances_actives; i-- > 0;) {
-        mvwprintw(win_providers, j, 2, "%d", an->prov_inst[i].sa);
+    for (uint8_t i = an->providers.reg.instances_actives_count; i-- > 0;) {
+        mvwprintw(win_providers, j, 2, "%d", an->providers.entries[i].sa);
         j++;
     }
 
@@ -117,11 +119,15 @@ void ui_draw(Analyzer *an) {
     box(win_trames, 0, 0);
     mvwprintw(win_trames, 0, 2, "PGN");
     int l = 1;
-    for (uint8_t k = an->pgn_mgmt.instances_actives; k-- > 0;) {
-        mvwprintw(win_trames, l, 2, "%d", an->pgn_inst[k].frame.pgn);
-        int m = 10;
-        for (uint8_t i = 0; i < an->pgn_inst[k].frame.data_len; i++) {
-            mvwprintw(win_trames, l, m, "%02X", an->pgn_inst[k].frame.data[i]);
+    for (uint8_t k = an->pgns.reg.instances_actives_count; k-- > 0;) {
+        mvwprintw(win_trames, l, 2, "%d", an->pgns.entries[k].frame.pgn);
+        mvwprintw(win_trames, l, 11, "%d", an->pgns.entries[k].provider);
+        mvwprintw(win_trames, l, 16, "%d",
+                  an->pgns.entries[k].metr.datas.tram_rate);
+        int m = 20;
+        for (uint8_t i = 0; i < an->pgns.entries[k].frame.data_len; i++) {
+            mvwprintw(win_trames, l, m, "%02X",
+                      an->pgns.entries[k].frame.data[i]);
             m += 3;
         }
         l++;
